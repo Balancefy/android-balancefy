53ef671a94bd23f9c3a8918abd4353807d321043
Manocchio <guruu.leste@gmail.com>
2022-11-10 11:49:41 -0300

    Merge branch 'feature-forum' of C:\Users\guruu\balancefy\android-balancefy with conflicts.

--- app/src/main/java/com/balancefy/balancefyapp/frames/ForumFragment.kt
+++ app/src/main/java/com/balancefy/balancefyapp/frames/ForumFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.balancefy.balancefyapp.databinding.FragmentForumBinding
+import com.google.android.material.tabs.TabLayout
 
 class ForumFragment : Fragment() {
     private lateinit var binding: FragmentForumBinding
@@ -16,5 +17,27 @@ class ForumFragment : Fragment() {
     ): View? {
         binding = FragmentForumBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        //tu fez o adapter do comentario já né wes????
+        //responde no chat pq eu to mutado
+        //
+
+        super.onViewCreated(view, savedInstanceState)
     }
 }
