02c5710ed37adb54c445e3206b27ccea34f035b6
Manocchio <guruu.leste@gmail.com>
2022-11-13 15:26:40 -0300

    add recycler view to general and posts

--- app/src/main/java/com/balancefy/balancefyapp/frames/ForumFragment.kt
+++ app/src/main/java/com/balancefy/balancefyapp/frames/ForumFragment.kt
@@ -57,7 +57,6 @@ class ForumFragment : Fragment() {
             }
         }}.attach()
 
-
         super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/balancefy/balancefyapp/frames/ForumGeralFragment.kt
+++ app/src/main/java/com/balancefy/balancefyapp/frames/ForumGeralFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.balancefy.balancefyapp.R
+import com.balancefy.balancefyapp.adapter.TopicPostsProfileAdapter
 import com.balancefy.balancefyapp.databinding.FragmentForumBinding
 import com.balancefy.balancefyapp.databinding.FragmentForumGeralBinding
 
@@ -19,6 +21,14 @@ class ForumGeralFragment : Fragment() {
     ): View? {
         binding = FragmentForumGeralBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerContainer = binding.recyclerContainer
+        recyclerContainer.layoutManager = LinearLayoutManager(context)
+        recyclerContainer.adapter = TopicPostsProfileAdapter(emptyList()){ println(it) }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
