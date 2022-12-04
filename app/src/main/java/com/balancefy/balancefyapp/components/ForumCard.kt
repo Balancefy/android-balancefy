d9acfe1fa673310e918eb5cd2983a40f52b94475
Ferreira-Mateus <mateus.andrade@bandtec.com.br>
2022-12-04 16:13:47 -0300

    Add post comment reply component

--- app/src/main/java/com/balancefy/balancefyapp/components/ForumCard.kt
+++ app/src/main/java/com/balancefy/balancefyapp/components/ForumCard.kt
@@ -1,12 +1,14 @@
 package com.balancefy.balancefyapp.components
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import com.balancefy.balancefyapp.R
 import com.balancefy.balancefyapp.databinding.ProfilePostCardBinding
+import java.net.URL
 
 class ForumCard(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
@@ -15,18 +17,15 @@ class ForumCard(context: Context, attrs: AttributeSet) : LinearLayout(context, a
     )
 
     init {
-        val attributes = context.obtainStyledAttributes(attrs, R.styleable.CurrentTask)
-
-        //setAvatar(attributes.getDrawable(R.styleable.ForumCard_forum_card_avatar) ?: R.drawable.ic_facebook)
-        setName(attributes.getString(R.styleable.ForumCard_forum_card_name) ?: "")
-        setTitle(attributes.getString(R.styleable.ForumCard_forum_card_title) ?: "")
-        setDescription(attributes.getString(R.styleable.ForumCard_forum_card_description) ?: "")
-        setLikes(attributes.getString(R.styleable.ForumCard_forum_card_likes) ?: "")
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.ForumCard)
+        /*val imageUrl = URL(attributes.getString(R.styleable.ForumCard_forum_card_avatar))
+        val photo = BitmapFactory.decodeStream(imageUrl.openConnection().getInputStream());
+        binding.postsProfileImage.setImageBitmap(photo)*/
+        setName(attributes.getString(R.styleable.ForumCard_forum_card_name) ?: "sem nome")
+        setTitle(attributes.getString(R.styleable.ForumCard_forum_card_title) ?: "sem titulo")
+        setDescription(attributes.getString(R.styleable.ForumCard_forum_card_description) ?: "sem descricao")
+        setLikes(attributes.getString(R.styleable.ForumCard_forum_card_likes) ?: "0")
     }
-
-    /*fun setAvatar(avatar: Any) {
-        binding.postsProfileImage.setImageURI(avatar)
-    }*/
 
     fun setName(name: String) {
         binding.tvPostsCreateProfileName.text = name
